37eb0da802e0b72386f62bc6215af9d57fa6ac7e
Angel <EMAIL>
2023-04-20 14:36:11 -0400

    app css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import TransactionList from "./Components/TransactionList";
 import TransactionForm from "./Components/TransactionForm";
+import "./App.css"; 
 
 function App() {
   return (
     <>
       <h1>Budget App</h1>
-      <TransactionList />
-      <TransactionForm />
+      <div className="container">
+        <div className="form-container">
+          <TransactionForm />
+        </div>
+        <div className="list-container">
+          <TransactionList />
+        </div>
+      </div>
     </>
   );
 }
